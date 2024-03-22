d209a46b0b2ed662195f108984d78d1bb1e0a530
gxhao <gxhao@epoint,com.cn>
2024-03-23 06:41:23 +0800

    【需求】：添加war包部署兼容

--- src/main/java/com/xiao/demo/DemoApplication.java
+++ src/main/java/com/xiao/demo/DemoApplication.java
@@ -3,12 +3,14 @@ package com.xiao.demo;
 import com.xiao.demo.Async.AnnotationSync.AnnotationSyncService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
@@ -17,5 +19,13 @@ public class DemoApplication {
         bean.sync();
         System.out.println("Async Stop");
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(DemoApplication.class);
+    }
+
 
 }
